6eb3dfb3a5c1b2d98feae67ec7e9c619a9e660d4
JijoGeorge2020 <129250005+JijoGeorge2020@users.noreply.github.com>
2024-01-05 20:07:14 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized
+
+# ### Objectives
+
+#         * Phyron
+#         * Sql
+#         * R
+#         
+
+# Some of the popular languages that Data Scientists use are:
+
+#     1.Phyton
+#     2.SQL
+#     3.R
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+#     1.NumPy
+#     2.Keras
+#     3.Pandas
+#     4.PyTorch
+
+# | Data Science Tools |
+# |:------------------:|
+# | 1.Apache Spark     |  
+# | 2.Jupiter Notebook | 
+# | 3.Julia            |  
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[1]:
+
+
+minutes=200
+hours=minutes/60
+print(hours)
+
+
+# ## Author
+
+# Jijo George
